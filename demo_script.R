cc71de29c416828dc9dc0e18818c9d51817f0685
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-03-13 13:10:48 -0500

    Written out mostly vector data analysis

--- demo_script.R
+++ demo_script.R
@@ -3,6 +3,10 @@ library(terra)
 library(tidyterra)
 
 theme_set(theme_bw())
+
+# Set up a color palette so that we don't have to copy it into our code so many times
+pal <- c("#F7FCF0","#E0F3DB","#CCEBC5","#A8DDB5","#7BCCC4","gray","#4EB3D3","#2B8CBE","#0868AC","#084081")
+
 #### Section 1: Vector Data ####
 
 ## Read in data
@@ -23,7 +27,7 @@ ggplot() +
 # Try again to make it look better
 ggplot() +
   geom_spatvector(data=elc, aes(fill=DOMVEG)) +
-  scale_fill_manual(values=c("#F7FCF0","#E0F3DB","#CCEBC5","#A8DDB5","#7BCCC4","gray","#4EB3D3","#2B8CBE","#0868AC","#084081")) +
+  scale_fill_manual(values=pal) +
   geom_spatvector(data=trails, color="darkred", linewidth=1) +
   geom_spatvector(data=camp, shape=21, size=3, fill="white") +
   theme_void() 
@@ -53,7 +57,7 @@ w_camps <- elc[elc$poly_id %in% w_camps,]
 # Note that the order of geoms defines drawing order, camps are on top to be able to see
 ggplot() +
   geom_spatvector(data=elc, aes(fill=DOMVEG)) +
-  scale_fill_manual(values=c("#F7FCF0","#E0F3DB","#CCEBC5","#A8DDB5","#7BCCC4","gray","#4EB3D3","#2B8CBE","#0868AC","#084081")) +
+  scale_fill_manual(values=pal) +
   geom_spatvector(data=trails, color="darkred", linewidth=1) +
   geom_spatvector(data=w_camps, fill=NA, color="hotpink", linewidth=1) +
   geom_spatvector(data=camp, shape=21, size=3, fill="white") +
@@ -61,17 +65,78 @@ ggplot() +
 
 ### Spatial analysis
 
-# Create a 50 m buffer around the trails
+## Create a 50 m buffer around the trails
 tr_buff <- buffer(trails, width=50, capstyle="round", joinstyle="round")
 
 # Note that there is not an argument in the buffer() function to dissolve, so we have to do that separately
 plot(tr_buff) # quick look
 tr_buff <- aggregate(tr_buff, dissolve=TRUE)
 
-# another quick look to see the difference
-plot(tr_buff)
+# Visualize on full map
+ggplot() +
+  geom_spatvector(data=elc, aes(fill=DOMVEG)) +
+  scale_fill_manual(values=pal) +
+  geom_spatvector(data=trails, color="black", linewidth=1) +  # Note the change in line color for better visualization
+  geom_spatvector(data=camp, shape=21, size=3, fill="white") +
+  geom_spatvector(data=tr_buff, color="red", fill=NA, linewidth=0.5) +
+  theme_void() +
+  theme(legend.position="none") # Dropping legend for this plot so that the map is bigger
+
+## Create a buffer around the campgrounds, but let campground capacity determine how big the buffer will be
+
+# Add a column to the camp point vector
+camp$buff_size <- ifelse(camp$CAPACITY <= 10, 100, 200)
+values(camp) # Check it worked
+
+# create buffers
+ca_buff <- buffer(camp, width=camp$buff_size)
+plot(ca_buff) # check it worked
+
+## Combine all of the buffers into a single layer
+buffunion <- union(tr_buff, ca_buff)
+
+# Again we need to aggregate and dissolve our buffers
+buffunion <- aggregate(buffunion, dissolve=TRUE)
+
+## How much area is potentially disturbed by the trails and campsites?
+disturbed_m <- expanse(buffunion) # Note that this returns values in m^2
+
+# convert to km2
+disturbed_km <- disturbed_m / 1000 / 1000
+disturbed_km
+
+# Visualize on full map
+ggplot() +
+  geom_spatvector(data=elc, aes(fill=DOMVEG)) +
+  scale_fill_manual(values=pal) +
+  geom_spatvector(data=trails, color="black", linewidth=1) +  # Note the change in line color for better visualization
+  geom_spatvector(data=camp, shape=21, size=3, fill="white") +
+  geom_spatvector(data=buffunion, color="red", fill=NA, linewidth=0.5) +
+  theme_void() +
+  theme(legend.position="none")
+
+### Now let's see how this disturbance impacts wolverine habitat
+
+
+
+
+
+
 
 
 #### Section 2: Raster Data ####
+
+## Import data. Note that some of these are vectors that we may have used already.
+# Ecological Land Classification (i.e., ecoregions) 
+elc <- vect("efb650_data/elc.shp")
+# Digital elevation model 
+dem <- rast("efb650_data/dem.tif")
+# Rivers
+rivers <- vect("efb650_data/rivers.shp")
+
+
+
+
+
 
 
